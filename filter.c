079915f6742ab51c0736f9ca8c98265fd6851977
RushanS <seif.rushan@gmail.ru>
2017-05-13 15:05:32 +0300

    add threads

--- filter.c
+++ filter.c
@@ -1,10 +1,11 @@
-//_REENTRANT
+#define _REENTRANT
 
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <sys/time.h>
 
 
 FILE *input_img;
@@ -13,9 +14,9 @@ int col, row;
 unsigned char r[1000][1000];
 unsigned char g[1000][1000];
 unsigned char b[1000][1000];
-int r_img[1000][1000];
-int g_img[1000][1000];
-int b_img[1000][1000];
+unsigned char img[1000][1000];
+pthread_t threads[10];
+
 
 void error(int c)
 {
@@ -23,18 +24,26 @@ void error(int c)
 	exit(1);
 }
 
-int min(int a, int b)
+void *thread_example(void *arg)
 {
-	return a < b ? a : b;
+	int *p = (int*)arg;
+	printf("i am thread #%d\n", *p);
+	sleep(2);
+	pthread_exit("goodbuy");
 }
 
 void main(int argc, char **argv)
-{
-	if (argc != 3) 
+{	
+	struct timespec t1, t2;
+	int threads_n;
+	
+	if (argc != 4) 
 		error(1);
 		
 	input_img = fopen(argv[1], "r");
 	output_img = fopen(argv[2], "w");
+	threads_n = argv[3][0] - '0';
+	
 	char basket[100];
 	fgets(basket, 100, input_img);
 	if (fgetc(input_img) == '#')
@@ -53,6 +62,9 @@ void main(int argc, char **argv)
 		}
 	}
 	
+	clock_gettime(CLOCK_REALTIME, &t1);
+	printf("start at %ld\n", t1.tv_nsec);
+	
 	for (int i = 1; i < row-1; i++) {
 		for (int j = 1; j < col-1; j++) {
 			int rx = r[i+1][j-1] + 2*r[i+1][j] + r[i+1][j+1] - r[i-1][j-1] - 2*r[i-1][j] - r[i-1][j+1];
@@ -65,21 +77,32 @@ void main(int argc, char **argv)
 			double gg = sqrt(gx*gx + gy*gy);
 			double gb = sqrt(bx*bx + by*by);
 			double color = (gr + gg + gb) / 3;
-			r_img[i][j] = color > 255 ? 255 : color;
+			img[i][j] = color > 255 ? 255 : color;
 		}
+	}	
+	clock_gettime(CLOCK_REALTIME, &t2);
+	printf("end at   %ld\n", t2.tv_nsec);
+	printf("diff = %ld\n", t2.tv_nsec - t1.tv_nsec / 1000000);
+	
+	int params[9];
+	
+	for (int i = 0; i < threads_n; i++) {
+		params[i] = i;
+		pthread_create(&threads[i], NULL, thread_example, (void*)&params[i]);
+	}
+	
+	for (int i = 0; i < threads_n; i++) {
+		void* thread_result;
+		int res = pthread_join(threads[i], &thread_result);
+		printf("thread #%d return %s\n", i, (char*)thread_result);
 	}
 	
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
-			fputc(r_img[i][j], output_img);
+			fputc(img[i][j], output_img);
 		}
 	}
-
 }
-
-
-
-
 
 
 
